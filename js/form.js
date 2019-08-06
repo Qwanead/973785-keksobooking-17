761749592df92fb9c470256676ff282499c4a519
Qwanead <qwanead@protonmail.com>
2019-08-06 11:25:35 +0500

    Личный проект: пока все дома. Часть 2

--- js/form.js
+++ js/form.js
@@ -23,6 +23,91 @@
     return pin;
   };
 
+  var renderCard = function (pin) {
+    var card = document.querySelector('#card').content.querySelector('.map__card');
+    var avatar = card.querySelector('.popup__avatar');
+    var title = card.querySelector('.popup__title');
+    var address = card.querySelector('.popup__text--address');
+    var price = card.querySelector('.popup__text--price');
+    var type = card.querySelector('.popup__type');
+    var capacity = card.querySelector('.popup__text--capacity');
+    var time = card.querySelector('.popup__text--time');
+    var description = card.querySelector('.popup__description');
+
+    avatar.src = pin.author.avatar;
+    title.textContent = pin.offer.title;
+    address.textContent = pin.offer.address;
+    price.textContent = pin.offer.price + '\u20bd' + '/ночь';
+    description.textContent = pin.offer.description;
+
+
+    switch (pin.offer.type) {
+      case 'flat':
+        type.textContent = 'Квартира';
+        break;
+      case 'bungalo':
+        type.textContent = 'Бунгало';
+        break;
+      case 'house':
+        type.textContent = 'Дом';
+        break;
+      case 'palace':
+        type.textContent = 'Дворец';
+        break;
+    }
+
+    capacity.textContent = pin.offer.rooms + ' комнаты для ' + pin.offer.guests + ' гостей';
+    time.textContent = 'Заезд после ' + pin.offer.checkin + ', выезд до ' + pin.offer.checkout;
+
+    var featuresList = card.querySelector('.popup__features');
+    var wifi = featuresList.querySelector('.popup__feature--wifi');
+    var dishwasher = featuresList.querySelector('.popup__feature--dishwasher');
+    var parking = featuresList.querySelector('.popup__feature--parking');
+    var washer = featuresList.querySelector('.popup__feature--washer');
+    var elevator = featuresList.querySelector('.popup__feature--elevator');
+    var conditioner = featuresList.querySelector('.popup__feature--conditioner');
+
+    while (featuresList.lastChild) {
+      featuresList.removeChild(featuresList.lastChild);
+    }
+
+    pin.offer.features.forEach(function (feature) {
+      switch (feature) {
+        case 'wifi':
+          featuresList.appendChild(wifi);
+          break;
+        case 'dishwasher':
+          featuresList.appendChild(dishwasher);
+          break;
+        case 'parking':
+          featuresList.appendChild(parking);
+          break;
+        case 'washer':
+          featuresList.appendChild(washer);
+          break;
+        case 'elevator':
+          featuresList.appendChild(elevator);
+          break;
+        case 'conditioner':
+          featuresList.appendChild(conditioner);
+          break;
+      }
+    });
+
+    var photosList = card.querySelector('.popup__photos');
+    var photo = photosList.querySelector('.popup__photo');
+
+    photosList.removeChild(photo);
+
+    pin.offer.photos.forEach(function (srcPhoto) {
+      var insertedPhoto = photo.cloneNode();
+      insertedPhoto.src = srcPhoto;
+      photosList.appendChild(insertedPhoto);
+    });
+
+    document.querySelector('.map__filters-container').insertAdjacentElement('beforebegin', card);
+  };
+
   var removePins = function () {
     var pins = document.querySelector('.map__offers');
 
@@ -55,6 +140,7 @@
   var onLoad = function (offers) {
     loadedOffers = offers;
     document.querySelector('.map__pins').appendChild(renderPins(loadedOffers));
+    renderCard(offers[0]);
   };
 
   var onError = function (response) {
